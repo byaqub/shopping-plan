826c3d75bde559960734bcc885607f6c7b03f100
byaqub <yaqub.benjamin@gmail.com>
2019-02-22 14:59:37 +0100

    initial commit

---
+++ src/main/java/yaqub/springframework/shoppingplan/ShoppingPlanApplication.java
@@ -0,0 +1,13 @@
+package yaqub.springframework.shoppingplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingPlanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingPlanApplication.class, args);
+    }
+
+}
